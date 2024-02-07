9006e4d5c679d407899e942ec9495ec6f38f92dd
김세정 <sejeong@sejeong-ui-MacBookPro.local>
2024-02-08 01:07:31 +0900

    feat: web - configured CORS

---
+++ src/main/java/com/example/demo/global/config/web/WebConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.global.config.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**") //허용 url
+                .allowedOrigins("*") //허용 경로
+                .allowedMethods( //허용 메소드
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.PATCH.name(),
+                        HttpMethod.DELETE.name(),
+                        HttpMethod.OPTIONS.name()
+                )
+                .maxAge(3600);
+    }
+}
