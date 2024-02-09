13a04ee2f800e2b8abebdebeaeaa73f642a3a494
sejyom <akxxkd01@gmail.com>
2024-02-10 02:33:56 +0900

    feat: global error handling

---
+++ demo-common/src/main/java/com/example/demo/global/error/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.example.demo.global.error;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    TEST(HttpStatus.INTERNAL_SERVER_ERROR, "001", "business-exception-test");
+
+    private HttpStatus httpStatus;
+    private String errorCode;
+    private String message;
+
+    ErrorCode(HttpStatus httpStatus, String errorCode, String errorMessage) {
+        this.httpStatus = httpStatus;
+        this.errorCode = errorCode;
+        this.message = errorMessage;
+    }
+}
---
+++ demo-common/src/main/java/com/example/demo/global/error/exception/BusinessException.java
@@ -0,0 +1,15 @@
+package com.example.demo.global.error.exception;
+
+import com.example.demo.global.error.ErrorCode;
+import lombok.Getter;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+}
